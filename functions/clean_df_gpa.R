d4d38484b96ab8b07fc19ce8787a6cffa41847eb
Mason Chen <mas.chen6345@gmail.com>
2016-08-08 14:15:16 -0700

    added random files

--- functions/clean_df_gpa.R
+++ functions/clean_df_gpa.R
@@ -1,5 +1,6 @@
 library(readr)
 library(dplyr)
+library(stringr)
 
 clean_df_gpa = function(path) {
     df = read_csv("../raw_data/fall2010gpa.csv.gz") %>%
@@ -9,7 +10,14 @@ clean_df_gpa = function(path) {
               'instr_name', 'grade_pts', 'department', 'course_num', 'sec_num', 
               'letter_grade_subtype', 'grade_type', 'letter_grade', 'course_title')
     names(df) = names
-    View(df)
-    #fix missing grade points
     
+    #fix missing grade points
+    grade_pt_mapping = c('A\\+$' = 4.3, 'A$' = 4.0, 'A-$' = 3.7,
+                         'B\\+$' = 3.3, 'B$' = 3.0, 'B-$' = 2.7,
+                         'C\\+$' = 2.3, 'C$' = 2.0, 'C-$' = 1.7,
+                         'D\\+$' = 1.3, 'D$' = 1.0, 'D-$' = 0.7,
+                         'F$' = 0)
+    df = df %>%
+        select(-1, -3, -4) %>%
+        mutate(grade_pts = str_replace_all(letter_grade, grade_pt_mapping), grade_pts = as.numeric(grade_pts))
 }
