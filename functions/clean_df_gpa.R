aa227b600608aeff8dd8772fa9e6a925016a6d13
Mason Chen <mas.chen6345@gmail.com>
2016-08-08 13:16:25 -0700

    paper template added

--- functions/clean_df_gpa.R
+++ functions/clean_df_gpa.R
@@ -2,14 +2,14 @@ library(readr)
 library(dplyr)
 
 clean_df_gpa = function(path) {
-    path = "~/Downloads/fall2015gpa.csv"
-    df = read_csv(path)
+    df = read_csv("../raw_data/fall2010gpa.csv.gz") %>%
+        select(-1)
     #fix col names
-    names = c('grade_id', 'grade_count', 'ccn', 'dirty_col1', 'dirty_col2',
+    names = c('grade_id', 'grade_count', 'dirty_col1', 'dirty_col2', 'ccn', 
               'instr_name', 'grade_pts', 'department', 'course_num', 'sec_num', 
               'letter_grade_subtype', 'grade_type', 'letter_grade', 'course_title')
     names(df) = names
-    
+    View(df)
     #fix missing grade points
     
 }
