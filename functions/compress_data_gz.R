2f1414c3c66ee7cb5d205ea215b180ca1ee96922
Mason Chen <mas.chen6345@gmail.com>
2016-08-09 00:21:54 -0700

    plots gpa

--- functions/compress_data_gz.R
+++ functions/compress_data_gz.R
@@ -5,5 +5,5 @@ library(readr)
 setwd("../raw_data/")
 files = list.files()
 files_to_compress = files[str_detect(files, "\\.csv$")]
-walk(files_to_compress, function(x) write.csv(read_csv(x), file=gzfile(str_c(x, '.gz')), row.names = F))
+walk(files_to_compress, function(x) write.csv(read_csv(x), file = gzfile(str_c(x, '.gz')), row.names = F))
 file.remove(files_to_compress)
