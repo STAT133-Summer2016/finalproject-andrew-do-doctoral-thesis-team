24676f0e4ed24525b87c6f4a60093919c44db566
iankatz <iankatz@berkeley.edu>
2016-08-08 21:06:36 -0700

    fixing summarizing functions

--- functions/summarizing_functions.R
+++ functions/summarizing_functions.R
@@ -1,36 +1,36 @@
-summarizer <- function(x) {
+summarizer <- function(df, x) {
   # Returns a data frame with two columns: x, and
   # the percentage breakdown of the school by x
   
-  return_df <- degrees %>% 
+  return_df <- df %>% 
     mutate(total = sum(count)) %>% 
-    group_by(degrees[[x]]) %>% 
+    group_by(df[[x]]) %>% 
     summarise(sum(count / total))
   names(return_df) <- c(x, "percent_of_total")
-  return(return_df)  
+  return(return_df)
 }
 
-detailed_summarizer <- function(x, y) {
+detailed_summarizer <- function(df, x, y) {
   # Returns a data frame with three columns: x, y, and
   # the percentage breakdown of y by x
   
-  return_df <- degrees %>% 
-    group_by(degrees[[y]]) %>% 
+  return_df <- df %>% 
+    group_by(df[[y]]) %>% 
     mutate(total = sum(count)) %>% 
     ungroup %>% 
-    group_by(degrees[[x]], degrees[[y]]) %>% 
+    group_by(df[[x]], df[[y]]) %>% 
     summarise(sum(count / total)) %>% 
     ungroup
   names(return_df) <- c(x, y, str_c("percent_of_", y))
   return(return_df)
 }
 
-comparison_summarizer <- function(x, y){
+comparison_summarizer <- function(df, x, y){
   # Returns a data frame with four columns: x, y, the
   # percentage breakdown of y by x, and the percentage
   # breakdown of the school by x
   
-  total_x <- summarizer(x)
-  x_by_y <- detailed_summarizer(x, y)
+  total_x <- summarizer(df, x)
+  x_by_y <- detailed_summarizer(df, x, y)
   left_join(x_by_y, total_x)
 }
