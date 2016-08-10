5146901bda5cc346eec89a163841f979eefbbb21
iankatz <iankatz@berkeley.edu>
2016-08-09 20:55:25 -0700

    adding to final_paper

--- clean_data/CleanData.R
+++ clean_data/CleanData.R
@@ -14,6 +14,12 @@ gpa_data = do.call(rbind, dfs)
 
 write.csv(gpa_data, file = gzfile(str_c('gpa_data.csv', '.gz')), row.names = F)
 
+ethnic_rollup_key <- read_csv("../raw_data/ethnic_rollup_key.csv.gz") %>% 
+  select(8, 9) %>% 
+  unique
+colnames(ethnic_rollup_key) <- c("ethnic_category",
+                                 "ethnicity")
+
 degrees <- read_csv("../raw_data/degrees.csv.gz") %>% 
   filter(!is.na(`Semester Year Letter Cd Concat`) &
            `Degree Level Desc` == "Bachelor") %>% 
@@ -26,7 +32,8 @@ degrees <- read_csv("../raw_data/degrees.csv.gz") %>%
          major = Major,
          count = `Student Headcount`) %>% 
   mutate(year = str_extract(year, "[0-9]{4}") %>% as.integer,
-         residency = str_extract(residency, "^[^\\s]+"))
+         residency = str_extract(residency, "^[^\\s]+")) %>% 
+  left_join(ethnic_rollup_key)
 
 write.csv(degrees, file = gzfile(str_c('degree_data.csv', '.gz')), row.names = F)
 
@@ -43,6 +50,8 @@ colnames(grad_time) <- c("year_semester",
 grad_time <- grad_time %>% 
   separate(col = year_semester, into = c("year", "semester"), sep = " ") %>% 
   mutate(year = year %>% as.integer,
-         time_to_grad = str_extract(time_to_grad, "[.0-9]+") %>% as.numeric)
+         residency = str_extract(residency, "^[^\\s]+"),
+         time_to_grad = str_extract(time_to_grad, "[.0-9]+") %>% as.numeric) %>% 
+  left_join(ethnic_rollup_key)
 
 write.csv(grad_time, file = gzfile(str_c('grad_time_data.csv', '.gz')), row.names = F)
